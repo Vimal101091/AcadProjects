95722ffe05933b9e734a64bf54b73aca40e72250
Vimal101091 <si.vimal@outlook.com>
2021-06-27 14:32:05 +0530

    RTOS Project

---
+++ ProjectRTOS/middleware/src/kernel/smartos/msp430f5529/.tmp/startup.msp430f5529.d
@@ -0,0 +1,119 @@
+.tmp/startup.msp430f5529.o: startup.c \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\kernel.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\macros.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\ostypes.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\osarch.h \
+ c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include-fixed\limits.h \
+ c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include\stdint.h \
+ c:\ti\msp430_gcc\msp430-elf\include\stdint.h \
+ c:\ti\msp430_gcc\msp430-elf\include\machine\_default_types.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\features.h \
+ c:\ti\msp430_gcc\msp430-elf\include\_newlib_version.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\_intsup.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\_stdint.h \
+ c:\ti\msp430_gcc\msp430-elf\include\stdio.h \
+ c:\ti\msp430_gcc\msp430-elf\include\_ansi.h \
+ c:\ti\msp430_gcc\msp430-elf\include\newlib.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\config.h \
+ c:\ti\msp430_gcc\msp430-elf\include\machine\ieeefp.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\cdefs.h \
+ c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include\stddef.h \
+ c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include\stdarg.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\reent.h \
+ c:\ti\msp430_gcc\msp430-elf\include\_ansi.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\_types.h \
+ c:\ti\msp430_gcc\msp430-elf\include\machine\_types.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\lock.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\types.h \
+ c:\ti\msp430_gcc\msp430-elf\include\machine\types.h \
+ c:\ti\msp430_gcc\msp430-elf\include\sys\stdio.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\version.h \
+ c:\ti\msp430_gcc\include\msp430.h c:\ti\msp430_gcc\include\msp430f5529.h \
+ c:\ti\msp430_gcc\include\iomacros.h c:\ti\msp430_gcc\include\in430.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\debug.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\debug_macros.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\libs\printfx.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\arch\sysclk.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\drv\uart.h \
+ e:\tugraz\rtos\rtos_public_ss17\middleware\include\arch\uart.h \
+ c:\ti\msp430_gcc\include\msp430f5529.h
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\kernel.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\macros.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\ostypes.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\osarch.h:
+
+c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include-fixed\limits.h:
+
+c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include\stdint.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\stdint.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\machine\_default_types.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\features.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\_newlib_version.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\_intsup.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\_stdint.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\stdio.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\_ansi.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\newlib.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\config.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\machine\ieeefp.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\cdefs.h:
+
+c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include\stddef.h:
+
+c:\ti\msp430_gcc\lib\gcc\msp430-elf\6.2.1\include\stdarg.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\reent.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\_ansi.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\_types.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\machine\_types.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\lock.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\types.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\machine\types.h:
+
+c:\ti\msp430_gcc\msp430-elf\include\sys\stdio.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\version.h:
+
+c:\ti\msp430_gcc\include\msp430.h:
+
+c:\ti\msp430_gcc\include\msp430f5529.h:
+
+c:\ti\msp430_gcc\include\iomacros.h:
+
+c:\ti\msp430_gcc\include\in430.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\debug.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\kernel\debug_macros.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\libs\printfx.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\arch\sysclk.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\drv\uart.h:
+
+e:\tugraz\rtos\rtos_public_ss17\middleware\include\arch\uart.h:
+
+c:\ti\msp430_gcc\include\msp430f5529.h:
